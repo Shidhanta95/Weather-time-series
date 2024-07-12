455a152fa13eb19795d9ec6021a411e5a819485b
Shidhanta95 <147026855+Shidhanta95@users.noreply.github.com>
2024-07-12 16:17:20 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,6 @@
 import  pandas as pd
 
-def loading_data():
+def data_load():
     data = pd.read_csv('testset.csv', index_col='datetime_utc')
     data.index = pd.to_datetime(data.index)
     return data
