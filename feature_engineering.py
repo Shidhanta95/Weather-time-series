4a56ac842f5fbe5687a42fe1f4aa2632d469956e
Shidhanta95 <147026855+Shidhanta95@users.noreply.github.com>
2024-07-12 16:09:38 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -5,6 +5,20 @@ from scipy import stats
 from sklearn.preprocessing import LabelEncoder
 from pandas.api.types import is_numeric_dtype
 
+def data_preprocess():
+    data = data_analysis()
+    data.rename(columns={' _conds': 'conditions', ' _dewptm': 'dewpoint',
+                     ' _fog': 'fog', ' _hail': 'hail', ' _heatindexm': 'heatindex', ' _hum': 'humidity',
+                     ' _precipm': 'precipitation', ' _pressurem': 'pressure', ' _rain': 'rain', ' _snow': 'snow',
+                     ' _tempm': 'temp', ' _thunder': 'thunder', ' _tornado': 'tornado', ' _vism': 'visibility',
+                     ' _wdird': 'wdirdegrees', ' _wdire': 'winddirection', ' _wgustm': 'windgust',
+                     ' _windchillm': 'windchill', ' _wspdm': 'windspeed'}, inplace=True)
+    data.drop(columns=['precipitation', 'windchill', 'heatindex', 'windgust'], inplace=True)
+    print(f'dataset shape (rows, columns) - {data.shape}')
+    data = data.replace(to_replace = -9999, value = np.nan)
+    data.ffill(inplace=True)
+    print(data[data.isnull()].count())
+    return data
 def feature_engineering():
 
     data = data_preprocess()
